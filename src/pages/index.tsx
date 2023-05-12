ab48afb055b0b8d5d81c34253b50ddc956b090fe
VladimirSukhoparov <sukhoparov.v.n@gmail.com>
2023-05-13 01:42:35 +0500

    skeleton

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import styles from '../styles/index.module.scss'
+
+
+export default function Home() {
+  return (
+    <div className={styles.main}>
+  <h1>hello</h1>
+    </div>
+  )
+}
